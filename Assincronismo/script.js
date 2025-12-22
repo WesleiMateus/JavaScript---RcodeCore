7a3c836648ade045954f9706128523eac28aff50
Weslei Mateus <wesleyhprozao@gmail.com>
2025-12-22 18:22:19 -0400

    Pra pegar o codigo la no trampo e ficar morcegando

---
+++ Assincronismo/script.js
@@ -0,0 +1,33 @@
+const showUser = document.createElement("div")
+showUser.classList.add("container")
+
+const mostrarNaTela = document.createElement("p")
+
+showUser.appendChild(mostrarNaTela)
+document.body.appendChild(showUser)
+
+const showInfosBtn = document.getElementById("btn");
+
+showInfosBtn.addEventListener("click", () => {
+    function getUser() {
+        return new Promise((res, rej) => {
+            setTimeout(() => {
+                res({ name: "weslei", age: 20 }),
+                    rej("501 - Internal Server Error")
+            }, 3000)
+        })
+    }
+
+    getUser()
+        .then((res) => {
+            mostrarNaTela.innerText = JSON.stringify(res);
+            console.log(JSON.stringify(res))
+        })
+        .catch((err) => {
+            mostrarNaTela.innerText = err;
+
+        })
+})
+
+
+
